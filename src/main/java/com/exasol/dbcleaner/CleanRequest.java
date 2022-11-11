efa35b0cefb75a58229d6a24735b73ebac87c7ea
Marco Nätlitz <marco.naetlitz@exasol.com>
2022-11-11 10:11:40 +0100

    Added JavaDoc for CleanRequest and CleanRequestBuilder

--- src/main/java/com/exasol/dbcleaner/CleanRequest.java
+++ src/main/java/com/exasol/dbcleaner/CleanRequest.java
@@ -1,11 +1,19 @@
 package com.exasol.dbcleaner;
 
+/**
+ * A request enabling the user to enable or disable the drop of certain database objects
+ * while using {@link ExasolDatabaseCleaner}.
+ */
 public final class CleanRequest {
     private final boolean cleanObjects;
     private final boolean cleanConnections;
     private final boolean cleanUsers;
     private final boolean cleanRoles;
 
+    /**
+     * Create an instance of {@link CleanRequestBuilder}
+     * @return instance of {@link CleanRequestBuilder}
+     */
     public static CleanRequestBuilder builder() {
         return new CleanRequestBuilder();
     }
@@ -17,22 +25,42 @@ public final class CleanRequest {
         this.cleanRoles = cleanRoles;
     }
 
+    /**
+     * Should all database objects be dropped?
+     * @return true if all objects should be dropped
+     */
     public boolean isCleanObjects() {
         return cleanObjects;
     }
 
+    /**
+     * Should all connections be dropped?
+     * @return true if all connections should be dropped
+     */
     public boolean isCleanConnections() {
         return cleanConnections;
     }
 
+    /**
+     * Should all user be dropped?
+     * @return true if users should be dropped
+     */
     public boolean isCleanUsers() {
         return cleanUsers;
     }
 
+    /**
+     * Should all roles be dropped?
+     * @return true if all roles should be dropped
+     */
     public boolean isCleanRoles() {
         return cleanRoles;
     }
 
+    /**
+     * Should everything be dropped?
+     * @return true if everything should be dropped
+     */
     public boolean isCleanAll() {
         return cleanObjects && cleanConnections && cleanUsers && cleanRoles;
     }
