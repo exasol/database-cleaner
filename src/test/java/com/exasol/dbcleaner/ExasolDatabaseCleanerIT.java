70555d4398614db2cc49754f124556d0104c0755
Marco Nätlitz <marco.naetlitz@exasol.com>
2022-11-11 09:49:40 +0100

    Extended Integration Test ExasolDatabaseCleanerIT

--- src/test/java/com/exasol/dbcleaner/ExasolDatabaseCleanerIT.java
+++ src/test/java/com/exasol/dbcleaner/ExasolDatabaseCleanerIT.java
@@ -40,10 +40,45 @@ class ExasolDatabaseCleanerIT {
     }
 
     @Test
+    void testPurgeSchema_userSpecified() throws SQLException {
+        createSchema();
+
+        final CleanRequest request = CleanRequest.builder()
+                .setCleanConnections(false)
+                .setCleanRoles(false)
+                .setCleanUsers(false)
+                .setCleanConnections(false)
+                .setCleanObjects(true)
+                .build();
+
+        CLEANER.cleanDatabase(request);
+        assertDoesNotThrow(this::createSchema);
+    }
+
+    @Test
     void testPurgeTable() throws SQLException {
         createSchema();
         createTable();
         CLEANER.cleanDatabase();
+        createSchema();
+        assertDoesNotThrow(this::createTable);
+    }
+
+    @Test
+    void testPurgeTable_userSpecified() throws SQLException {
+        createSchema();
+        createTable();
+
+        final CleanRequest request = CleanRequest.builder()
+                .setCleanConnections(false)
+                .setCleanRoles(false)
+                .setCleanUsers(false)
+                .setCleanConnections(false)
+                .setCleanObjects(true)
+                .build();
+
+        CLEANER.cleanDatabase(request);
+
         createSchema();
         assertDoesNotThrow(this::createTable);
     }
@@ -56,6 +91,22 @@ class ExasolDatabaseCleanerIT {
     }
 
     @Test
+    void testPurgeConnection_userSpecified() throws SQLException {
+        createConnection();
+
+        final CleanRequest request = CleanRequest.builder()
+                .setCleanConnections(true)
+                .setCleanRoles(false)
+                .setCleanUsers(false)
+                .setCleanConnections(false)
+                .setCleanObjects(false)
+                .build();
+
+        CLEANER.cleanDatabase(request);
+        assertDoesNotThrow(this::createConnection);
+    }
+
+    @Test
     void testPurgeUser() throws SQLException {
         createUser();
         CLEANER.cleanDatabase();
@@ -63,9 +114,42 @@ class ExasolDatabaseCleanerIT {
     }
 
     @Test
+    void testPurgeUser_userSpecified() throws SQLException {
+        createUser();
+
+        final CleanRequest request = CleanRequest.builder()
+                .setCleanConnections(false)
+                .setCleanRoles(false)
+                .setCleanUsers(true)
+                .setCleanConnections(false)
+                .setCleanObjects(false)
+                .build();
+
+        CLEANER.cleanDatabase(request);
+        assertDoesNotThrow(this::createUser);
+    }
+
+    @Test
     void testPurgeRole() throws SQLException {
         createRole();
         CLEANER.cleanDatabase();
+        assertDoesNotThrow(this::createRole);
+    }
+
+    @Test
+    void testPurgeRole_userSpecified() throws SQLException {
+        createRole();
+
+        final CleanRequest request = CleanRequest.builder()
+                .setCleanConnections(false)
+                .setCleanRoles(true)
+                .setCleanUsers(false)
+                .setCleanConnections(false)
+                .setCleanObjects(false)
+                .build();
+
+        CLEANER.cleanDatabase(request);
+
         assertDoesNotThrow(this::createRole);
     }
 
@@ -81,6 +165,22 @@ class ExasolDatabaseCleanerIT {
         createFunction("S1");
         createFunction("S2");
         CLEANER.cleanDatabase();
+        assertDoesNotThrow(() -> createFunction("S1"));
+    }
+
+    @Test
+    void testPurgeFunction_userSpecified() throws SQLException {
+        createFunction("S1");
+
+        final CleanRequest request = CleanRequest.builder()
+                .setCleanConnections(false)
+                .setCleanRoles(false)
+                .setCleanUsers(false)
+                .setCleanConnections(false)
+                .setCleanObjects(true)
+                .build();
+
+        CLEANER.cleanDatabase(request);
         assertDoesNotThrow(() -> createFunction("S1"));
     }
 
